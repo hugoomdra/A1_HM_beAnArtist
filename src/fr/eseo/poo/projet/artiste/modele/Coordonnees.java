365992a7bd809b84255745858a5d2a53588dd79b
Hugo Madureira <hugoomdra@gmail.com>
2020-03-31 18:09:11 +0200

    first commit

---
+++ src/fr/eseo/poo/projet/artiste/modele/Coordonnees.java
@@ -0,0 +1,93 @@
+package fr.eseo.poo.projet.artiste.modele;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+public class Coordonnees {
+
+	public static final double ABSCISSE_PAR_DEFAUT = 0;
+	public static final double ORDONNEE_PAR_DEFAUT = 0;
+
+	private double abscisse;
+	private double ordonnee;
+
+	public Coordonnees() {
+		this(ABSCISSE_PAR_DEFAUT, ORDONNEE_PAR_DEFAUT);
+	}
+
+	public Coordonnees(double abscisse, double ordonnee) {
+		setAbscisse(abscisse);
+		setOrdonnee(ordonnee);
+	}
+
+	public double getAbscisse() {
+		return this.abscisse;
+	}
+
+	public void setAbscisse(double abscisse) {
+		this.abscisse = abscisse;
+	}
+
+	public double getOrdonnee() {
+		return this.ordonnee;
+	}
+
+	public void setOrdonnee(double ordonnee) {
+		this.ordonnee = ordonnee;
+	}
+
+	/**
+	 * Fonction qui permet de déplacer la coordonnée à un autre endroit.
+	 * 
+	 * @param abscisse La nouvelles abscisse de la coordonnees.
+	 * @param ordonnee La nouvelles ordonnees de la coordonnees.
+	 */
+	public void deplacerVers(double abscisse, double ordonnee) {
+		this.setAbscisse(abscisse);
+		this.setOrdonnee(ordonnee);
+	}
+
+	/**
+	 * 
+	 * Fonction qui permet de déplacer la coordonnée en fonction des arguments
+	 * 
+	 * @param deltaAbscisse déplacement en X
+	 * @param deltaOrdonnee déplacement en Y
+	 */
+	public void deplacerDe(double deltaAbscisse, double deltaOrdonnee) {
+		this.setAbscisse(this.getAbscisse() + deltaAbscisse);
+		this.setOrdonnee(this.getOrdonnee() + deltaOrdonnee);
+	}
+
+	/**
+	 * 
+	 * Fonction qui permet de connaitre la distance deux points.
+	 * 
+	 * @param coord une coordonnée
+	 * @return la distance entre la coordonnées et la coordonnée donnée en argument
+	 */
+	public double distanceVers(Coordonnees coord) {
+		return (Math.sqrt(Math.pow(coord.getAbscisse() - this.getAbscisse(), 2)
+				+ Math.pow(coord.getOrdonnee() - this.getOrdonnee(), 2)));
+	}
+
+	/**
+	 * 
+	 * Fonction qui permet de connaitre l'angle entre deux points.
+	 * 
+	 * @param coord une coordonnée
+	 * @return l'angle entre la coordonnées et la coordonnée donnée en argument
+	 */
+	public double angleVers(Coordonnees coord) {
+		return Math.toDegrees(Math.atan(Math.abs(coord.getOrdonnee() - this.getOrdonnee())
+				/ Math.abs(coord.getAbscisse() - this.getAbscisse())));
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getNumberInstance(Locale.getDefault());
+		DecimalFormat df = (DecimalFormat) nf;
+		df.applyPattern("##.##");
+		return "(" + df.format(this.getAbscisse()) + " , " + df.format(this.getOrdonnee()) + ")";
+	}
+
+}
